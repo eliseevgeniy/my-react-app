7ecf27b49f44749d2dee1f7a2b9558472eae1641
Evgen <eliseevgeniy@gmail.com>
2026-09-25 12:52:47 +0700

    Компоненты в React

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-/*import logo from './logo.svg';
+import React, { useRef } from 'react';
+
+import Button from './components/Button';
+
+import Input from './components/Input';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;*/
-
-/*import React from 'react';
-
-import dayjs from 'dayjs';
 function App() {
 
-    const currentDate = dayjs().format('YYYY MM DD HH:mm:ss');
+    const inputRef = useRef(null);
+
+
+    const handleClear = () => {
+
+        if (inputRef.current) {
+
+            inputRef.current.value = '';
+
+        }
+
+    };
+
 
     return (
 
-        <div>
+        <div className="app-container">
 
-            <h1>Текущая дата и время:</h1>
+            <Input ref={inputRef} placeholder="Введите текст..." />
 
-            <p>{currentDate}</p>
+            <Button onClick={handleClear} />
 
         </div>
 
@@ -45,30 +37,5 @@ function App() {
 
 }
 
-
-export default App;*/
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css'; // Обычно добавляется в CRA
-
-const App = () => { // 1. Обернули в компонент
-    // 2. Объявили состояние (изначально null, так как данных еще нет)
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts/1')
-            .then(response => setData(response.data))
-            .catch(error => console.error(error));
-    }, []);
-
-    return (
-        <div>
-            <h1>Данные из API:</h1>
-            {/* 3. Логика отображения осталась верной */}
-            {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Загрузка...</p>}
-        </div>
-    );
-};
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Button.css';
+
+const Button = ({ onClick }) => {
+
+    return (
+
+        <button className="custom-button" onClick={onClick}>
+
+            Очистить
+
+        </button>
+
+    );
+
+};
+
+export default Button;
